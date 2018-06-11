af620ac0b9e4b833744d8267d4cef17947603c52
Unknown <dchaika@inbox.ru>
2018-06-11 15:58:41 +0300

    start fix

--- assets/scripts/forms.js
+++ assets/scripts/forms.js
@@ -20,15 +20,25 @@ $(document).ready(function () {
             }
         };
 
+        /*
+        *На сообщение об ошибке вешается .invalid
+        * Если checkbox || radio .invalid вешаеться на родителя .group-required
+         */
         var showValidateError = function ( html, text ) {
             html.classList.add('invalid');
             var error = generateError( text );
             html.parentElement.appendChild(error);
         };
 
+        /*
+        *Поиск форм происходит по классу .required, только их валидатор будет просматривать
+         */
+
         for ( var i = 0; i < form.length; i++ ) {
             form[i].addEventListener('submit', function (e) {
                 e.preventDefault();
+                var submit = true;
+
                 var requiredForm = [].slice.call( this.querySelectorAll('.required') );
 
                 // Delete validate
@@ -43,8 +53,15 @@ $(document).ready(function () {
 
                     if ( !requiredForm[i].value ) {
                         showValidateError( requiredForm[i], 'Required field' );
+                        submit = false;
                     }
                 }
+
+                /*
+                *Если есть форма паспорта и повторения паспорта
+                * На форме паспорта должен быть .password
+                * На форме повторения паспорта .passwordConfirmation
+                 */
 
                 if ( this.querySelector('.password') && this.querySelector('.passwordConfirmation') ) {
                     var password = this.querySelector('.password');
@@ -54,20 +71,88 @@ $(document).ready(function () {
 
                     if ( password.value != passwordConfirmation.value ){
                         showValidateError( passwordConfirmation, 'Passwords does not match' );
+                        submit = false;
                     }
 
                 }
+
+                /*
+                * Если есть форма почты
+                * На форме почты должен быть .email
+                 */
 
                 if ( this.querySelector('.email') )  {
                     var email = this.querySelector('.email');
 
                     // Test email input
 
-                    if ( email.value.indexOf('@') < 1 || email.value.indexOf('.') < 1 ){
+                    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+                    if ( !re.test(String(email.value).toLowerCase()) ){
                         showValidateError( email, 'test@test.com' );
+                        submit = false;
                     }
                 }
 
+                /*
+                * Если есть форма checkbox
+                * На родители формы должен быть .checkbox-group.group-required
+                 */
+
+                if ( this.querySelector( 'input[type=checkbox]' ) ){
+
+                    // Test checkbox group
+
+                    var checkboxGroupArr = [].slice.call( this.querySelectorAll( '.checkbox-group.group-required' ) );
+
+                    for ( var i = 0; i < checkboxGroupArr.length; i++ ) {
+                        var key = false;
+                        var checkboxArr = [].slice.call( checkboxGroupArr[i].querySelectorAll('input[type=checkbox]') );
+
+                        checkboxArr.forEach( function (elem) {
+                            if ( elem.checked ){
+                                key = true;
+                            }
+                        } );
+
+                        if ( !key ){
+                            showValidateError( checkboxGroupArr[i], 'Required field' );
+                            submit = false;
+                        }
+
+                    }
+                }
+
+                /*
+                * Если есть форма radio
+                * На родители формы должен быть .radio-group.group-required
+                 */
+
+                if ( this.querySelector( 'input[type=radio]' ) ){
+
+                    // Test radio group
+
+                    var radioGroupArr = [].slice.call( this.querySelectorAll( '.radio-group.group-required' ) );
+
+                    for ( var i = 0; i < radioGroupArr.length; i++ ) {
+                        var key = false;
+                        var radioArr = [].slice.call( radioGroupArr[i].querySelectorAll('input[type=radio]') );
+
+                        radioArr.forEach( function (elem) {
+                            if ( elem.checked ){
+                                key = true;
+                            }
+                        } );
+
+                        if ( !key ){
+                            showValidateError( radioGroupArr[i], 'Required field' );
+                            submit = false;
+                        }
+
+                    }
+                }
+
+                if ( submit ) this.submit();
             })
         }
     }
